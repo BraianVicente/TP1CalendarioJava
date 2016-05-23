3efc1ea5603cfd2b88d582f2b32f3917cb59aa43
BraianVicente <bvicente@fi.uba.ar>
2016-05-23 19:27:21 -0300

    Agregados diagramas UML y clases e interfaz

---
+++ src/fiuba/algo3/modelo/Recurso.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fiuba.algo3.modelo;
+
+/**
+ *
+ * @author brahvic
+ */
+public class Recurso implements Invitable {
+
+    @Override
+    public boolean existeEvento(int anio, int mes, int dia, int hora) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean agregarEvento(int anio, int mes, int dia, int hora) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean estaOcupado(int anio, int mes, int dia, int hora) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
