625d6ae6c96428aedd167cfc0114802552a43825
BraianVicente <bvicente@fi.uba.ar>
2016-05-26 01:30:26 -0300

    modificando agregar eventos semanales

--- test/fiuba/algo3/tests/ContenedorDePersonasTests.java
+++ test/fiuba/algo3/tests/ContenedorDePersonasTests.java
@@ -6,6 +6,7 @@
 package fiuba.algo3.tests;
 
 import fiuba.algo3.modelo.ContenedorDePersonas;
+import java.util.ArrayList;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -24,4 +25,27 @@ public class ContenedorDePersonasTests {
         Assert.assertTrue(cont.existe("Juan")) ;
     }    
     
+    @Test
+    public void test02agregarEvento(){
+        ContenedorDePersonas cont = new ContenedorDePersonas();
+        cont.agregar("Juan");
+        ArrayList inv = new ArrayList();
+        inv.add("Juan");
+        cont.agregarEvento("unEvento", inv, 2016, 5, 6, 15);
+        Assert.assertTrue(cont.estaOcupado("Juan", 2016, 5, 6, 15));
+    }
+    
+    @Test
+    public void test03agregarEventoSemanal(){
+        ContenedorDePersonas cont = new ContenedorDePersonas();
+        cont.agregar("Juan");
+        ArrayList inv = new ArrayList();
+        inv.add("Juan");
+        cont.agregarEvento(2,"unEvento", inv, 2016, 5, 6, 15);
+        Assert.assertTrue(cont.estaOcupado("Juan", 2016, 5, 13, 15));
+        Assert.assertTrue(cont.estaOcupado("Juan", 2016, 5, 6, 15));
+
+    }
+    
 }
+    
